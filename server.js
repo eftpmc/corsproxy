482647d44069d919807ae47db8b5477b841d019a
eftpmc <zackhhi@gmail.com>
2023-06-23 10:43:50 -0400

    cors

--- server.js
+++ server.js
@@ -5,7 +5,11 @@ const axios = require('axios');
 const app = express();
 const port = 3000;
 
-app.use(cors());
+const corsOptions = {
+  origin: ['http://localhost:5173', 'https://aritools.vercel.app/'],
+};
+
+app.use(cors(corsOptions));
 
 app.get('/', async (req, res) => {
   const videoUrl = req.query.url;
